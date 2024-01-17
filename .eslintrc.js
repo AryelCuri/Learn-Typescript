3aca21e8e9a511a50c616fa208e11ee15db439f7
ariel curi <aryelcuri2@gmail.com>
2024-01-17 10:03:59 -0300

    chore: add config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,14 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "standard-with-typescript",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  env: {
+      node: true,
+  },
+  extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+      '@typescript-eslint',
+      'import'
+  ],
 }
