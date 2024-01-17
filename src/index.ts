95a384c437b82c37251598015d7e2097d0536876
ariel curi <aryelcuri2@gmail.com>
2024-01-17 10:23:52 -0300

    chore: add capitulo1 exercicios 1 e 2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
-export const app = (): void => {
-  console.log('Hello World!')
+const capitulo1_exercicio1 = (): void => {
+  console.log('capitulo 1.1 facil Crie uma função que imprima "Olá, mundo!" no console')
+  console.log('Ola, Mundo!')
+
+
+}
+const capitulo1_exercicio2 = (param: string): void => {
+  console.log('capitulo1.2 facil Escreva uma função que aceite um parâmetro e o imprima no console.')
+  console.log(param)
+
 }
 
+
+
+const main = (): void => {
+  
+  capitulo1_exercicio1()
+  capitulo1_exercicio2('param')
+}
+main()
