6be794b7f289a7e0d0036c977763e5476a509a3e
AryelCuri <aryelcuri2@gmail.com>
2024-01-18 14:32:53 -0300

    chore: add capitulo 5 exercicio 3

--- src/index.ts
+++ src/index.ts
@@ -246,6 +246,24 @@ enum MesesDoAno {
   console.log(MesesDoAno)
 } 
 
+const capitulo5_exercicio3 = (): void => {
+  console.log('capitulo 5.2 Fácil: Utilize um enum para representar os meses do ano')
+  
+enum PontosCardeais {
+  Norte = 'Norte',
+  Nordeste = 'Nordeste',
+  Leste = 'Leste',
+  Sudeste = 'Sudeste',
+  Sul = 'Sul',
+  Sudoeste = 'Sudoeste',
+  Oeste = 'Oeste',
+  Noroeste ='Noroeste'
+ 
+
+}
+console.log(PontosCardeais)
+}   
+
 const capitulo9_exercicio1 = (): void =>{
   console.log('capitulo 9.1 Fácil: Crie um loop que imprima os números de 1 a 10.')
   let i: number = 1
@@ -471,7 +489,8 @@ const main = (): void => {
   //capitulo4_exercicio7(['a', 'adfs', 'asr'])
 
   //capitulo5_exercicio1()
-  capitulo5_exercicio2()
+  //capitulo5_exercicio2()
+  capitulo5_exercicio3()
 
 
 
